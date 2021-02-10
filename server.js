ecd4b0c7001896f93d3b405eab71c29e6a297f88
Daniel Sasser <desasser79@gmail.com>
2021-02-09 21:17:46 -0800

    working on displaying a chosen workout

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.set("views", path.join(__dirname, 'views'));
 const db = require("./models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 
@@ -38,33 +38,65 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
 // ROUTER
 // ================================================================================
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   db.Workout.find()
-  .populate('exercises')
-  .then(workoutDb => {
-    // const hbsObj = {
-    //   workouts : workoutDb
-    // }
-    const data = JSON.stringify(workoutDb);
-    res.render('index', JSON.parse(data))
-  }).catch(err => {
-    console.log(err);
-    res.send(err);
-  })
+    .populate('exercises')
+    .then(workoutDb => {
+      // const hbsObj = {
+      //   workouts : workoutDb
+      // }
+      const data = JSON.stringify(workoutDb);
+      res.render('index', JSON.parse(data))
+    }).catch(err => {
+      console.log(err);
+      res.send(err);
+    })
 })
 
 // RENDER: NEW WORKOUT
-app.get('/newworkout', (req,res) => {
+app.get('/newworkout', (req, res) => {
   res.render('./partials/newWorkout')
 })
 
 // RENDER: NEW EXERCISE
-app.get('/newexercise', (req,res) => {
-  res.render('./partials/newExercise')
+app.get('/newexercise', (req, res) => {
+  db.Workout.find().sort({ _id: -1 }).limit(5).lean().then(workoutsData => {
+    // console.log(workoutsData);
+    const hbsObj = {
+      workout: workoutsData
+    }
+    console.log(hbsObj);
+    res.render('./partials/newExercise', hbsObj)
+  })
+})
+
+// RENDER: ALL WORKOUTS
+app.get('/workouts', (req, res) => {
+  db.Workout.find().sort({ _id: -1 }).limit(5).lean().then(workoutsData => {
+    // console.log('raw data', workoutsData);
+    const hbsObj = {
+      workout: workoutsData
+    }
+    // console.log('fixed data', hbsObj);
+    res.render('./partials/allWorkouts', hbsObj)
+  })
+})
+
+// RENDER: SPECIFIC WORKOUT
+app.get('/activeworkout/:id', (req, res) => {
+  db.Workout.find({_id:mongojs.ObjectId(req.params.id)})
+    .populate('exercises')
+    .then(workoutDb => {
+      res.json(workoutDb)
+    }).catch(err => {
+      console.log(err);
+      res.send(err);
+    })
+  // console.log('workout body', req.params);
 })
 
 //SEE ALL EXERCISES
-app.get('/api/exercises', (req,res) => {
+app.get('/api/exercises', (req, res) => {
   db.Exercise.find().then(exerciseDb => {
     res.json(exerciseDb)
   }).catch(err => {
@@ -74,7 +106,7 @@ app.get('/api/exercises', (req,res) => {
 })
 
 //SEE ALL WORKOUTS
-app.get('/api/workouts', (req,res) => {
+app.get('/api/workouts', (req, res) => {
   db.Workout.find().then(workoutDb => {
     res.json(workoutDb)
   }).catch(err => {
@@ -84,44 +116,53 @@ app.get('/api/workouts', (req,res) => {
 })
 
 //SEE WORKOUT WITH EXERCISES
-app.get('/populatedworkouts', (req,res) => {
+app.get('/populatedworkouts', (req, res) => {
   db.Workout.find()
-  .populate('exercises')
-  .then(workoutDb => {
-    res.json(workoutDb)
-  }).catch(err => {
-    console.log(err);
-    res.send(err);
-  })
+    .populate('exercises')
+    .then(workoutDb => {
+      res.json(workoutDb)
+    }).catch(err => {
+      console.log(err);
+      res.send(err);
+    })
 })
 
 
 //CREATE NEW WORKOUT
 app.post('/newworkout', ({ body }, res) => {
   db.Workout.create(body)
-  .then(workoutDb => {
-    res.json(workoutDb);
-    res.redirect('/newexercise');
-  })
-  .catch(err => {
-    console.log(err);
-    res.send(err);
-  })
+    .then(workoutDb => {
+      res.redirect('/newexercise');
+      // res.json(workoutDb);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    })
 })
 
 //CREATE NEW EXERCISE
-app.post('/api/exercises', (req,res) => {
-  console.log(req.body);
-  
-  db.Exercise.create(req.body)
-  .then(exerciseDb => {
-    db.Workout.findOneAndUpdate({_id:req.body.workoutId}, {$push: {exercises: exerciseDb._id}})
-    .then(workoutDb => res.send(workoutDb))
+app.post('/newexercise', (req, res) => {
+  // console.log(req.body.workoutId[0]);
+
+  db.Exercise.create({
+    workoutId: req.body.workoutId[0],
+    name: req.body.name,
+    type: req.body.type,
+    weight: req.body.weight,
+    sets: req.body.sets,
+    reps: req.body.reps,
+    duration: req.body.duration,
+    distance: req.body.distance
   })
-  .catch(err => {
-    console.log(err);
-    res.send(err);
-  })
+    .then(exerciseDb => {
+      db.Workout.findOneAndUpdate({ _id: req.body.workoutId[0] }, { $push: { exercises: exerciseDb._id } })
+        .then(workoutDb => res.send(workoutDb))
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    })
 })
 
 // =============================================================================
